e3648bf9e2c3240e9ea8340816fec77e5988e3c9
adrian729 <adrian.sanchez.albanell@gmail.com>
2017-04-15 20:08:16 +0200

    anadido login y tablon anundios a views.py, modificadas 4 tonterias para probar un poco, nombres modificados para que sean coherentes (todos en ingles), hardcodeadas 4 tonterias para probar.

--- app/views.py
+++ app/views.py
@@ -5,3 +5,40 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    userTypes = [  # fake array of posts
+        { 
+            'name': 'Carpintero',
+            'description': 'Beautiful day in Portland!',
+            'id_tipo_usuario': 1
+        },
+        { 
+            'name': 'Fontanero', 
+            'description': 'To be or not to be!',
+            'id_tipo_usuario': 2
+        },
+        { 
+            'name': 'Malabarista', 
+            'description': 'Lore ipsum',
+            'id_tipo_usuario': 3
+        }
+    ]
+    return render_template('login.html',
+    	                   userTypes=userTypes)
+
+@app.route('/bulletin_board')
+def bulletin_board():
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'},
+            'body': 'Busco guitarra en Escocia!'
+        },
+        { 
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('bulletin_board.html',
+                           posts=posts)
